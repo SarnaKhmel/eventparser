e97eb3729989a040b46c7b791cbf9b0fad721183
SarnaKhmel <olsarnat@gmail.com>
2020-05-19 15:24:47 +0300

    2 new class and add parseItem, context

---
+++ app/src/main/java/com/example/eventparser/ParseAdapter.java
@@ -0,0 +1,55 @@
+package com.example.eventparser;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ParseAdapter extends RecyclerView.Adapter<ParseAdapter.ViewHolder> {
+
+    private ArrayList<ParseItem> parseItems;
+    private Context context;
+
+    public ParseAdapter(ArrayList<ParseItem> parseItems, Context context){
+        this.parseItems = parseItems;
+        this.context = context;
+
+    }
+
+    @NonNull
+    @Override
+    public ParseAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.parse_item, parent, false);
+        return  new ViewHolder(view) ;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ParseAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return parseItems.size();
+    }
+
+    public class ViewHolder extends  RecyclerView.ViewHolder{
+
+        ImageView imageView;
+        TextView textView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+            textView = itemView.findViewById(R.id.TextView);
+
+        }
+    }
+}
